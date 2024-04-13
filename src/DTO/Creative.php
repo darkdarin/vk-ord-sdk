cc5e04ac76a90cdc9fd50a1b68fbf5dbbb3bd75e
darkdarin <darkdarin@tochka.com>
2024-04-13 16:25:43 +0400

    feat: add person_external_id field to Creative DTO

--- src/DTO/Creative.php
+++ src/DTO/Creative.php
@@ -8,10 +8,11 @@ namespace DarkDarin\VkOrdSdk\DTO;
 readonly class Creative
 {
     /**
-     * @param string $contract_external_id Внешний идентификатор изначального договора, для которого создается креатив
      * @param list<string> $okveds Список кодов ОКВЭД креатива
      * @param CreativePayTypeEnum $pay_type Модель оплаты показа креатива
      * @param CreativeFormEnum $form Форма распространения креатива
+     * @param string|null $contract_external_id Внешний идентификатор изначального договора, для которого создается креатив. Если это поле заполнено, то id контрагента должен отсутствовать.
+     * @param string|null $person_external_id Внешний идентификатор контрагента, для которого создается креатив саморекламы. Если это поле заполнено, то id контракта должен отсутствовать.
      * @param string|null $name Название креатива. Используется для вашего удобства
      * @param string|null $brand Бренд рекламируемых товаров или услуг. Допускается ввести несколько брендов через точку
      * @param string|null $category Вид рекламируемых товаров или услуг. Допускается ввести несколько видов через точку
@@ -24,10 +25,11 @@ readonly class Creative
      * @param list<CreativeFlagEnum> $flags Информация о том, что креатив относится к особому типу рекламного объявления
      */
     public function __construct(
-        public string $contract_external_id,
         public array $okveds,
         public CreativePayTypeEnum $pay_type,
         public CreativeFormEnum $form,
+        public ?string $contract_external_id = null,
+        public ?string $person_external_id = null,
         public ?string $name = null,
         public ?string $brand = null,
         public ?string $category = null,
