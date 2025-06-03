58b4f00d08a480900f7696fb8c2e65b46f94fb64
Pavel Babushkin <idarksoft@gmail.com>
2025-06-03 15:32:43 +0200

    fix: Поправил имя поля в InvoiceV3Item

--- src/DTO/InvoiceV3Item.php
+++ src/DTO/InvoiceV3Item.php
@@ -8,12 +8,12 @@ namespace DarkDarin\VkOrdSdk\DTO;
 readonly class InvoiceV3Item
 {
     /**
-     * @param string $external_id Внешний идентификатор изначального договора.
+     * @param string $contract_external_id Внешний идентификатор изначального договора.
      * @param DetailedAmount $amount Информация о сумме разаллокации по изначальному договору.
      * @param list<InvoiceV3ItemCreative>|null $creatives Список креативов изначального договора в разаллокации.¬
      */
     public function __construct(
-        public string $external_id,
+        public string $contract_external_id,
         public DetailedAmount $amount,
         public ?array $creatives = null,
     ) {}
